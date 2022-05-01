b8b8a6f78da2e31e4bbe16ce13e6943d66e9dc65
Bilel <b.oueslati@ewm.swiss>
2022-05-01 15:52:36 +0200

    Initial commit

---
+++ public/build/app.js
@@ -0,0 +1,34 @@
+"use strict";
+(self["webpackChunkumanlink"] = self["webpackChunkumanlink"] || []).push([["app"],{
+
+/***/ "./assets/app.js":
+/*!***********************!*\
+  !*** ./assets/app.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _style_app_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style/app.css */ "./assets/style/app.css");
+
+
+/***/ }),
+
+/***/ "./assets/style/app.css":
+/*!******************************!*\
+  !*** ./assets/style/app.css ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/app.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7QUNBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3VtYW5saW5rLy4vYXNzZXRzL2FwcC5qcyIsIndlYnBhY2s6Ly91bWFubGluay8uL2Fzc2V0cy9zdHlsZS9hcHAuY3NzPzlhYzMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFwiLi9zdHlsZS9hcHAuY3NzXCI7IiwiLy8gZXh0cmFjdGVkIGJ5IG1pbmktY3NzLWV4dHJhY3QtcGx1Z2luXG5leHBvcnQge307Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    
+    './templates/**/*.twig',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
